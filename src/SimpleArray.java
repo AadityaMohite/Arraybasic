66d04d6b470ae1eaaed8c6686607c813d8795b6c
Aaditya_coder❤️ <mohiteaadi10@gmail.com>
2025-11-26 01:37:15 +0530

    added new file

--- src/SimpleArray.java
+++ src/SimpleArray.java
@@ -13,7 +13,7 @@ public class SimpleArray {
             arr[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
 
