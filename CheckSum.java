e0a32a5f0520a5556551dffec4bab441bac203a6
Aaditya_coder❤️ <mohiteaadi10@gmail.com>
2025-12-06 23:31:45 +0530

    initial commit

---
+++ CheckSum.java
@@ -0,0 +1,41 @@
+
+
+import java.util.Scanner;
+
+public class CheckSum {
+    public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int n = sc.nextInt();
+
+           int [] arr = new int[n];
+
+        System.out.println("Enter the Elments of an array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the Target Sum : ");
+        int target = sc.nextInt();
+
+        boolean Result = check(arr,target);
+        System.out.println(Result);
+
+    }
+   static boolean check(int[]arr,int target) {
+
+       for (int i = 0; i < arr.length; i++) {
+           for (int j = i + 1; j < arr.length; j++) {
+
+               if (arr[i] + arr[j] == target) {
+                   return  true;
+               }
+
+
+           }
+       }
+
+      return  false;
+   }
+
+}
